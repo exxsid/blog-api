1b7c339d06c9d71a01773254b2deace4a4db3671
exxsid <cortezleoanthony5@gmail.com>
2024-08-28 08:19:37 +0800

    init commit

---
+++ src/test/java/com/leoanthony/bloggingplatformapi/BloggingplatformapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leoanthony.bloggingplatformapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloggingplatformapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
